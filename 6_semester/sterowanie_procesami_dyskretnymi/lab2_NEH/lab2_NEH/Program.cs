a4c63f23e5f25615d8bf2a65b7bbd2c5bf2c37e3
Spawek <spawnerer@gmail.com>
2013-04-07 17:03:01 +0200

    final NEH script added

--- 6_semester/sterowanie_procesami_dyskretnymi/lab2_NEH/lab2_NEH/Program.cs
+++ 6_semester/sterowanie_procesami_dyskretnymi/lab2_NEH/lab2_NEH/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Threading;
 using System.IO;
 using System.Runtime.CompilerServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace lab2_NEH
 {
@@ -145,35 +146,6 @@ namespace lab2_NEH
         {
             int[,] arr = new int[currList.Count + 1, taskToInsert.subtasks.Count + 1];
 
-            //LinkedListNode<Task> bestPosToInsertBefore = null;
-            //int bestPosCMax = int.MaxValue;
-            //foreach (Task task in currList)
-            //{
-            //    LinkedList<Task> tempList = new LinkedList<Task>(currList); //linked list copying in here - low performance action //doesnt look so bad after performance analysis
-            //    LinkedListNode<Task> nodeToInsertBefore = tempList.Find(task); //linear complexity - bad!!!
-            //    tempList.AddBefore(nodeToInsertBefore, taskToInsert);
-            //    int currCMax = CalcCMaxforPermutation(tempList);
-            //    if (currCMax < bestPosCMax)
-            //    {
-            //        bestPosCMax = currCMax;
-            //        bestPosToInsertBefore = nodeToInsertBefore;
-            //    }
-            //}
-            //LinkedList<Task> tempList2 = new LinkedList<Task>(currList); //refactor this "2" shit
-            //LinkedListNode<Task> nodeToInsertAfter = tempList2.Last;
-            //tempList2.AddAfter(nodeToInsertAfter, taskToInsert);
-            //int currCMax2 = CalcCMaxforPermutation(tempList2);
-            //if (currCMax2 < bestPosCMax)
-            //{
-            //    currList.AddAfter(currList.Find(nodeToInsertAfter.Value), taskToInsert);
-            //}
-            //else
-            //{
-            //    currList.AddBefore(currList.Find(bestPosToInsertBefore.Value), taskToInsert);
-            //}
-
-
-
             LinkedListNode<Task> bestPosToInsertBefore = null;
             int bestPosCMax = int.MaxValue;
             LinkedListNode<Task> node = currList.First; //DOESNT WORK FOR SOME REASON  - will be much faster
@@ -198,34 +170,7 @@ namespace lab2_NEH
             }
         }
 
-        /// <summary>
-        /// create arr and return last elem (CMax)
-        /// </summary>
-        /// <param name="currList"></param>
-        /// <returns></returns>
-        //private static int CalcCMaxforPermutation(LinkedList<Task> currList)
-        //{
-        //    int noOfThingsToDoInEachTask = currList.First().subtasks.Count;
-        //    int[,] arr = new int[currList.Count + 1, noOfThingsToDoInEachTask + 1];
-
-        //    int i = 1;
-        //    int noOfThingsToDoInEachTaskPlus1 = noOfThingsToDoInEachTask + 1; //to improve performance
-        //    LinkedListNode<Task> currTask = currList.First;
-        //    while(currTask != null)
-        //    {
-        //        for (int j = 1; j < noOfThingsToDoInEachTaskPlus1; j++)
-        //        {
-        //            arr[i, j] = Math.Max(arr[i - 1, j], arr[i, j - 1]) + currTask.Value.subtasks[j - 1];
-        //        }
-                
-        //        i++;
-        //        currTask = currTask.Next;
-        //    }
-
-        //    return arr[i - 1, noOfThingsToDoInEachTask];
-        //}
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] //for optimization
         public static int CalcCMaxforPermutation(LinkedList<Task> currList)
         {
             int[] arr = new int[currList.First().subtasks.Count + 1];
@@ -272,8 +217,45 @@ namespace lab2_NEH
             return output;
         }
 
+        private enum Mode
+        {
+            standard, accelerated, unknown
+        }
+
+        private static Mode getModeFromUser()
+        {
+            Console.WriteLine("SELECT MODE:");
+            Console.WriteLine("\t1 = standard NEH mode");
+            Console.WriteLine("\t2 = accelerated NEH mode");
+            Console.WriteLine();
+
+            Mode mode = Mode.unknown;
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.D1:
+                    mode = Mode.standard;
+                    break;
+
+                case ConsoleKey.D2:
+                    mode = Mode.accelerated;
+                    break;
+
+                default:
+                    mode = Mode.unknown;
+                    break;
+            }
+
+            if (mode == Mode.unknown)
+            {
+                Console.WriteLine("Wrong mode!\n");
+                return getModeFromUser();
+            }
+            return mode;
+        }
         static void Main(string[] args)
         {
+            Mode mode = getModeFromUser();
+
             List<LinkedList<Task>> jobs = LoadDataFromFile("bench_fs.txt");
             List<List<int>> permutations = new List<List<int>>(jobs.Count);
             for (int i = 0; i < jobs.Count; i++)
@@ -282,11 +264,15 @@ namespace lab2_NEH
             }
 
             DateTime startTime = DateTime.Now;
-            
 
-            Parallel.For(0, jobs.Count, i =>
+            //map subtasks -> work time
+            Dictionary<int, List<TimeSpan>> statistics = new Dictionary<int, List<TimeSpan>>();
+            Object statsLock = new object();
+
+            //Parallel.For(0, jobs.Count, i =>            //PARALLEL MODE
+            for (int i = 0; i < jobs.Count; i++)      //SINGLE THREAD MODE
             {
-                //LinkedList<Task> orderedTasks = new LinkedList<Task>(tasks.OrderBy(x => -1 * x.subtasks.Sum()));
+                DateTime jobStartTIme = DateTime.Now;
                 LinkedList<Task> orderedTasks = new LinkedList<Task>(jobs[i].OrderBy(x => -1 * x.subtasks.Sum()));
 
                 LinkedList<Task> correctPermutation = new LinkedList<Task>();
@@ -295,15 +281,31 @@ namespace lab2_NEH
 
                 while (orderedTasks.Count > 0)
                 {
-                    //InsertTaskToGetLowestCMax(correctPermutation, orderedTasks.First());
-                    InsertTaskToGetLowestCMaxAccelerated(correctPermutation, orderedTasks.First());
+                    if (mode == Mode.standard)InsertTaskToGetLowestCMax(correctPermutation, orderedTasks.First());
+                    else if (mode == Mode.accelerated)InsertTaskToGetLowestCMaxAccelerated(correctPermutation, orderedTasks.First());
+                    else throw new ApplicationException("UNKNOWN MODE");
+
                     orderedTasks.RemoveFirst();
                 }
 
+                //stats saving
+                lock (statsLock)
+                {
+                    if (!statistics.ContainsKey(jobs[i].Count))
+                    {
+                        statistics.Add(jobs[i].Count, new List<TimeSpan>());
+                    }
+                    statistics[jobs[i].Count].Add(DateTime.Now - jobStartTIme);
+                }
+
                 Console.WriteLine("JOB NO: {0}", i + 1);
+                Console.WriteLine("SUBTASKS NO: {0}", jobs[i].Count);
+                Console.WriteLine("STAGES NO: {0}", jobs[i].First().subtasks.Count);
+                Console.WriteLine("THIS TASK TIME: {0}", DateTime.Now - jobStartTIme);
                 Console.WriteLine("Time elapsd: {0}", DateTime.Now - startTime);
-                //Console.WriteLine("C_Max = {0}", CalcCMaxforPermutation(correctPermutation));
+                Console.WriteLine("C_Max = {0}", CalcCMaxforPermutation(correctPermutation));
                 Console.WriteLine();
+
                 foreach (Task task in correctPermutation)
                 {
                     //Console.Write(task.taskNo + " ");
@@ -311,7 +313,7 @@ namespace lab2_NEH
                 }
                 Console.WriteLine("\n");
 
-            });
+            }//);
 
             using (StreamWriter sw = new StreamWriter("output.txt"))
             {
@@ -325,8 +327,90 @@ namespace lab2_NEH
                 }
             }
 
+            Console.WriteLine("\n\n\nTIME STATISTICS:");
+            Console.WriteLine("SUBTASKS  \t\tTIME [MS]");
+            foreach (int key in statistics.Keys)
+            {
+                Console.WriteLine("   {0}\t\t--->      {1}", key, statistics[key].Average(x => x.Milliseconds));
+            }
+            Console.WriteLine("\n\nTOTAL TIME: {0}", DateTime.Now - startTime);
+
+
             Console.WriteLine("Click any key to exit...");
             Console.ReadKey();
         }
     }
+
+    //TESTS PASTED HERE JUST TO USE ONLY 1 FILE
+    [TestClass]
+    public class UnitTest1
+    {
+        private LinkedList<Task> taskList;
+        private Task taskToInsert;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            taskList = new LinkedList<Task>();
+            taskList.AddLast(new Task(new List<int>() { 9, 4, 5, 8, 6 })); //z3
+            taskList.AddLast(new Task(new List<int>() { 5, 9, 8, 10, 1 })); //z1
+            taskList.AddLast(new Task(new List<int>() { 9, 3, 10, 1, 8 })); //z2
+
+            taskToInsert = new Task(new List<int>() { 4, 8, 8, 7, 2 });
+        }
+
+
+        [TestMethod]
+        public void EArrCreationTest()
+        {
+            int[,] actual = Program.CalcEArr(taskList);
+            int[,] expectedArr = new int[4, 6] { { 0, 0, 0, 0, 0, 0 }, { 0, 9, 13, 18, 26, 32 }, { 0, 14, 23, 31, 41, 42 }, { 0, 23, 26, 41, 42, 50 } };
+
+            CollectionAssert.AreEqual(expectedArr, actual);
+        }
+
+        [TestMethod]
+        public void QArrCreationTest()
+        {
+            int[,] actual = Program.CalcQArr(taskList);
+            int[,] expectedArr = new int[4, 6] { { 50, 40, 32, 27, 15, 0 }, { 41, 36, 27, 19, 9, 0 }, { 31, 22, 19, 9, 8, 0 }, { 0, 0, 0, 0, 0, 0 } };
+
+            CollectionAssert.AreEqual(expectedArr, actual);
+        }
+
+        [TestMethod]
+        public void FArrCreationTest()
+        {
+            int[,] actual = Program.CalcFArr(Program.CalcEArr(taskList), taskToInsert);
+            int[,] expectedArr = new int[4, 6] { { 0, 4, 12, 20, 27, 29 }, { 0, 13, 21, 29, 36, 38 }, { 0, 18, 31, 39, 48, 50 }, { 0, 27, 35, 49, 56, 58 } };
+
+            CollectionAssert.AreEqual(expectedArr, actual);
+        }
+
+        [TestMethod]
+        public void FPlusQCreationTest()
+        {
+            var actual = Program.AddArrays(
+                Program.CalcFArr(Program.CalcEArr(taskList), taskToInsert),
+                Program.CalcQArr(taskList)
+            );
+
+            int[,] expectedArr = new int[4, 6] { { 54, 52, 52, 54, 44, 0 }, { 54, 57, 56, 55, 47, 0 }, { 49, 53, 58, 57, 58, 0 }, { 27, 35, 49, 56, 58, 0 } };
+
+            CollectionAssert.AreEqual(expectedArr, actual);
+        }
+
+        [TestMethod]
+        public void FindBestColTest()
+        {
+            var FPlusQ = Program.AddArrays(
+                Program.CalcFArr(Program.CalcEArr(taskList), taskToInsert),
+                Program.CalcQArr(taskList)
+            );
+
+            int minCol = Program.FindMinColInFPlusQArr(FPlusQ);
+            Assert.AreEqual(0, minCol);
+        }
+    }
+
 }
