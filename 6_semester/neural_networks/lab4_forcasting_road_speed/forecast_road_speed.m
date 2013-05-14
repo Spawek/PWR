36087cb687bd44df07b0c64d99bee11340f0a43f
Spawek <spawnerer@gmail.com>
2013-05-14 12:22:54 +0200

    added testing data and learning on testing data added sprawozdanie.docx

--- 6_semester/neural_networks/lab4_forcasting_road_speed/forecast_road_speed.m
+++ 6_semester/neural_networks/lab4_forcasting_road_speed/forecast_road_speed.m
@@ -1,20 +1,18 @@
-function [ predictions ] = forecast_road_speed( training_data, work_data )
+function [ predictions ] = forecast_road_speed( training_data, real_data )
 
 %% constants declaration
 backward_data_usage_depth = 4; % app will use actual data this no backward
 road_you_need_speed_on = 3;
 how_far_forward_to_forecast = 3;
+minutes_per_step = 5;
 
 % no of times that every nauron will be recalculated
 % (neurons are recalculated in random permutation)
 learning_iterations = 500;
 
-%% fix data taken from speed.xls
-training_data = fix_input_data(training_data);
-
 %% calculating helpers
 no_of_roads = size(training_data, 1);
-training_data_points_no = size(training_data, 2) - (backward_data_usage_depth + 1);
+
 probe_size = no_of_roads * (backward_data_usage_depth + 1);
 
 %% creating 'random' weights vector
@@ -29,24 +27,43 @@ probes = generate_probes(training_data, backward_data_usage_depth);
 training_results = training_data(road_you_need_speed_on, backward_data_usage_depth+1+how_far_forward_to_forecast:end)';
 
 %% network learning!
+done = 0;
 for iterations = 1:learning_iterations*probe_size;
     w = make_learning_step(w, probes, training_results, how_far_forward_to_forecast);
+    
+    new_done = round(iterations*100/(learning_iterations*probe_size));
+    if(new_done ~= done)
+       done = new_done;
+       disp([num2str(done), '% done'])
+    end
 end
 
 %% tests
 predictions = make_predictions(w, probes);
 abs_error = abs((predictions(1:end-how_far_forward_to_forecast) - training_results));
 
-%predictions = [zeros(backward_data_usage_depth+1,1);predictions];
+%% for real data
+real_data = fix_input_data(real_data);
+probes = generate_probes(real_data, backward_data_usage_depth);
+predictions = make_predictions(w, probes);
+training_results = real_data(road_you_need_speed_on, backward_data_usage_depth+1+how_far_forward_to_forecast:end)';
+abs_error = abs((predictions(1:end-how_far_forward_to_forecast) - training_results));
 
 clf
 subplot(2,1,1)
 plot(predictions, 'r')
 hold on
-%plot(training_data(road_you_need_speed_on,:),'b')
 plot(training_results, 'b')
+
+title(['wartosc aktualna i predykcja na ', num2str(minutes_per_step*how_far_forward_to_forecast), ' minut'])
+legend('predykcja', 'wartosc aktualna')
 
 subplot(2,1,2)
 plot(abs_error);
+title('blad bezwzgledny')
+
+
+
+
 
 end
---
+++ 6_semester/neural_networks/lab4_forcasting_road_speed/prepare_training_data.m
@@ -0,0 +1,13 @@
+function [ training_data ] = prepare_training_data(  )
+
+%taking data from script
+dane
+
+training_data = [workingday1,workingday2,workingday3,workingday4,workingday5, ...
+    workingday6, workingday7, workingday8, workingday9, workingday10, ...
+    workingday11, workingday12, workingday13, workingday14, workingday15, ...
+    workingday16, workingday17, workingday18, workingday19, workingday20, ...
+    workingday21, workingday22, workingday23];
+
+end
+
