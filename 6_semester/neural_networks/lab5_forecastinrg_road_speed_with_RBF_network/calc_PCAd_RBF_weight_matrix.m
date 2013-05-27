78ef9da5f405aebaf3cd4cef14b340dbfbf6b312
Spawek <spawnerer@gmail.com>
2013-05-27 22:12:43 +0200

    finishing RBF road speed prediction

--- 6_semester/neural_networks/lab5_forecastinrg_road_speed_with_RBF_network/calc_PCAd_RBF_weight_matrix.m
+++ 6_semester/neural_networks/lab5_forecastinrg_road_speed_with_RBF_network/calc_PCAd_RBF_weight_matrix.m
@@ -1,7 +1,7 @@
-function [ weights_matrix, A_matrix, r, PCAd_probes, probes ] = calc_PCAd_RBF_weight_matrix( training_data, BACKWARD_DATA_USAGE )
+function [ weights_matrix, A_matrix, r, PCAd_probes] = calc_PCAd_RBF_weight_matrix( training_data, BACKWARD_DATA_USAGE )
 
 %% constants block
-PCA_CERTAINITY_LEVEL = 0.9997; % WTF? - i need certainity level like that to have 15 probes out of 30
+PCA_CERTAINITY_LEVEL = 0.9999999;% 0.9997; % WTF? - i need certainity level like that to have 15 probes out of 30
 
 %% probes preparation
 probes = generate_probes(training_data, BACKWARD_DATA_USAGE);
@@ -9,7 +9,7 @@ probes = generate_probes(training_data, BACKWARD_DATA_USAGE);
 %% PCA
 [PCAd_probes, A_matrix] = cut_dimensions(probes, PCA_CERTAINITY_LEVEL); 
 
-disp(['PCA is cutting dims from ', ...
+disp(['PCA is cutting dimensions from ', ...
     num2str(size(A_matrix, 1)), ' to ', ...
     num2str(size(A_matrix, 2)) ])
 
